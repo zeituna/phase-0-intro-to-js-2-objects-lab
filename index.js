d15c54d1fc7569c390bef5a298f872bc90f51155
leila <laila.ahmed.2645@gmail.com>
2026-01-17 19:05:36 +0300

    object-lab-done

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+const employee = {};
+
+/* NON-DESTRUCTIVE */
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+  const newEmployee = { ...employee };
+  newEmployee[key] = value;
+  return newEmployee;
+}
+
+/* DESTRUCTIVE */
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+  employee[key] = value;
+  return employee;
+}
+
+/* NON-DESTRUCTIVE */
+function deleteFromEmployeeByKey(employee, key) {
+  const newEmployee = { ...employee };
+  delete newEmployee[key];
+  return newEmployee;
+}
+
+/* DESTRUCTIVE */
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+  delete employee[key];
+  return employee;
+}
